5f84265aef28c08038d508f14f7c26eefc89dd02
abdhulla-k <abdhullak149@gmail.com>
2024-02-22 18:38:35 +0530

    created glowind card when mouse move on it

---
+++ glowing-card-mouse-move/script.js
@@ -0,0 +1,14 @@
+const cardContainer = document.querySelector(".clowing-cards");
+const cards = document.querySelectorAll(".clowing-cards .card");
+
+cardContainer.addEventListener('mousemove', e => {
+  cards.forEach(card => {
+    const react = card.getBoundingClientRect();
+
+    const x = e.clientX - react.left;
+    const y = e.clientY - react.top; // Corrected from react.right to react.top
+
+    card.style.setProperty("--x", `${x}px`)
+    card.style.setProperty("--y", `${y}px`)
+  })
+})
